92ee7768fa8ee02397e03c3a29bb9f2da2869e62
Felipe D. Santos <lycan.felipe@gmail.com>
2021-05-20 08:22:06 -0300

    Declaring Functions

--- JavaScript-TypeScript/Functions/declarandoFuncoes.js
+++ JavaScript-TypeScript/Functions/declarandoFuncoes.js
@@ -7,3 +7,23 @@ function falarOi()
 }
 falarOi();
 
+//First Class Object (Objeto de primeira classe)
+// Function Expression
+const souUmDado = function() {
+    console.log("Sou um dado");
+}
+souUmDado();
+
+//Arrow Function
+const funcaoArrow = () => {
+    console.log("Sou uma arrow function");
+}
+funcaoArrow();
+
+//Dentro de um objeto
+const obj = {
+    falar: function() {
+        console.log("Estou falando...");
+    }
+}
+obj.falar();
