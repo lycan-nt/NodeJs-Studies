2e62e18403a21deecd20a4667b1373f72a45a06b
Felipe D. Santos <lycan.felipe@gmail.com>
2021-05-20 06:10:52 -0300

    Paramns

---
+++ JavaScript-TypeScript/Params/params.js
@@ -0,0 +1,12 @@
+//Arguments que sustenta todos os argumentos enviados
+function funcao() {
+    let total = 0;
+
+    for (let  argumento of arguments)
+    {
+        total += argumento;
+    }
+
+    console.log(total);
+}
+funcao(1,2,3,4,5);
