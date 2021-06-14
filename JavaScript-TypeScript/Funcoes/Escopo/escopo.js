04104302522c5beb76eb8f25aa8ecf4490dfd886
lycan-nt <lycan.felipe@gmail.com>
2021-06-14 11:10:11 -0300

    Contexto

---
+++ JavaScript-TypeScript/Funcoes/Escopo/escopo.js
@@ -0,0 +1,14 @@
+const nome = "Felipe D. Santos";
+
+function falarNome() 
+{
+    const nome = "Marcella D. Santos";
+    console.log(nome);
+}
+falarNome();
+
+function usarFalarNome()
+{
+    falarNome();
+}
+usarFalarNome();
