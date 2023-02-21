59fb2f89c153800881c5b1e0f33393f73a9b3c36
Felipe D. Santos <lycan.felipe@gmail.com>
2023-02-20 22:22:06 -0300

    Create Tasks

--- Cursos/NestJS/Zero-To-Hero/nestjs-task-management/src/tasks/tasks.controller.ts
+++ Cursos/NestJS/Zero-To-Hero/nestjs-task-management/src/tasks/tasks.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
+import { Body, Post } from '@nestjs/common/decorators';
 import { Task } from './task.model';
 import { TasksService } from './tasks.service';
 
@@ -9,5 +10,13 @@ export class TasksController {
   @Get()
   getAllTasks(): Task[] {
     return this.tasksService.getAllTasks();
+  }
+
+  @Post()
+  createTask(
+    @Body('title') title: string,
+    @Body('description') description:string
+  ): Task {
+    return this.tasksService.createTask(title, description);
   }
 }
