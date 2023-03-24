547cd3c1995b94488fe98eef008ea3893008df14
Felipe D. Santos <lycan.felipe@gmail.com>
2023-03-24 06:03:34 -0300

    Error Handling: Username

--- Cursos/NestJS/Zero-To-Hero/nestjs-task-management/src/auth/users.repository.ts
+++ Cursos/NestJS/Zero-To-Hero/nestjs-task-management/src/auth/users.repository.ts
@@ -1,3 +1,4 @@
+import { ConflictException } from "@nestjs/common/exceptions";
 import { EntityRepository, Repository } from "typeorm";
 import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
 import { User } from "./user.entity";
@@ -10,6 +11,13 @@ export class UserRepository extends Repository<User> {
             userName,
             password
         });
-        await this.save(user);
+        try {
+            await this.save(user);
+        } catch (error) {
+            if (error.code === '23505') {
+                throw new ConflictException('Username already exists');
+            }
+            throw new Error(error.message);
+        }
     }
 }
