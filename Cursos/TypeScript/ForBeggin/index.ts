96b1d4e6836a4629e5a07f0b7ca39068c0383da8
Felipe D. Santos <lycan.felipe@gmail.com>
2023-11-28 05:41:33 -0300

    Tipo de dados: Primitivos

--- Cursos/TypeScript/ForBeggin/index.ts
+++ Cursos/TypeScript/ForBeggin/index.ts
@@ -1,1 +1,11 @@
+/** Tipo de dados: Primitivos **/
+
 let nome:string = "Felipe D. Santos";
+
+let idade:number = 28;
+
+let lgAtivo:boolean = true;
+
+let bizarro: undefined = undefined;
+
+let nada:null = null;
