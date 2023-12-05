627002eea78b91d19cbdb6c6c9e534aaf1e684b6
Felipe D. Santos <lycan.felipe@gmail.com>
2023-12-05 06:28:30 -0300

    Tests

--- Cursos/TypeScript/clean-arch/src/route.entity.spec.ts
+++ Cursos/TypeScript/clean-arch/src/route.entity.spec.ts
@@ -1,5 +1,30 @@
+import { Route, RouteProps } from "./route.entity"
+
 describe('Route Tests', () => {
     test('constructor', () => {
-        
+        let routeProps: RouteProps = {
+            title: 'Minha Rota',
+            startPosition: {lat:0, lng: 1},
+            endPoint: {lat: 2, lng: 3},
+        }
+        let route = new Route(routeProps);
+        expect(route.props).toStrictEqual({
+            ...routeProps, points: []
+        })
+
+        routeProps = {
+            title: 'Minha Rota',
+            startPosition: {lat: 0, lng: 1},
+            endPoint: {lat: 2, lng: 3},
+            points: [
+                {lat: 10, lng: 11}
+            ]
+        }
+        route = new Route(routeProps);
+        expect(route.props).toStrictEqual({
+            ...routeProps, points: [
+                {lat: 10, lng: 11}
+            ]
+        })
     })
 })
