5f9f7856a86f70587dd78532fadcb35dafd8d44c
Felipe D. Santos <lycan.felipe@gmail.com>
2023-12-05 22:02:15 -0300

    Express

--- Cursos/TypeScript/clean-arch/src/infra/http/express/index.ts
+++ Cursos/TypeScript/clean-arch/src/infra/http/express/index.ts
@@ -3,10 +3,17 @@ import { CreateRouteUseCase } from '../../../application/create-route.use-case';
 import { RouteInMemoryRepository } from '../../db/route-in-memory.repository';
 
 const app: Express = express();
+app.use(express.json)
 const repository = new RouteInMemoryRepository();
+const port = process.env.PORT || 3000;
 
 app.post('/routes', async (req: Request, res: Response) => {
     const createUseCase = new CreateRouteUseCase(repository);
     const output = await createUseCase.execute(req.body);
     res.status(201).json(output);
 });
+
+app.listen(port, () => {
+    console.log('Server On in port: ', port);
+})
+
