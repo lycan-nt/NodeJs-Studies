4bd0fed29f54156a470527d6118e2d330c6d7f28
Felipe D. Santos <lycan.felipe@gmail.com>
2023-12-07 21:20:17 -0300

    Post Videos

--- Cursos/Node/node-beggin/server.js
+++ Cursos/Node/node-beggin/server.js
@@ -2,9 +2,17 @@ import {fastify } from 'fastify';
 import { DatabaseMemory } from './database-memory.js';
 
 const server = fastify();
+const database = new DatabaseMemory();
 
-server.post('/videos', () => {
-
+server.post('/videos', (request, reply) => {
+    const { title, description, duration } = request.body;
+    database.create({
+        title: title,
+        description: description,
+        duration: duration
+    });
+    console.log(database.list());
+    return reply.status(201).send();
 });
 
 server.get('/videos', () => {
