e4dc758f5ee0a4a44d787bbb3c8c58fdfbfb0687
Felipe D. Santos <lycan.felipe@gmail.com>
2023-12-14 20:02:33 -0300

    nodemailer implement

---
+++ Cursos/Node/solid-api/src/providers/implementations/MailtrapMailProvider.ts
@@ -0,0 +1,35 @@
+import Mail from "nodemailer/lib/mailer";
+import { IMailProvider, IMessage } from "../IMailProvider";
+import  nodemailer from 'nodemailer';
+
+export class MailtrapMailProvider implements IMailProvider {
+
+    private transporter: Mail;
+
+    constructor() {
+        this.transporter = nodemailer.createTransport({
+            host: process.env.EMAILHOST,
+            port: Number(process.env.EMAILPORT),
+            auth: {
+                user: process.env.EMAILUSER,
+                pass:  process.env.EMAILPASS
+            }
+        });
+    }
+
+    async sendMail(message: IMessage): Promise<void> {
+        await this.transporter.sendMail({
+            to: {
+                name: message.to.name,
+                address: message.to.email
+            },
+            from: {
+                name: message.from.name,
+                address: message.from.email
+            },
+            subject: message.subject,
+            html: message.body
+        })
+    }
+    
+}
