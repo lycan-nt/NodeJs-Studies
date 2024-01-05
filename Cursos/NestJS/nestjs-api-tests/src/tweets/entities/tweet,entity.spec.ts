f8475aa9d90041022ac0f8dd6da0d7421940d778
Felipe D. Santos <lycan.felipe@gmail.com>
2024-01-04 22:13:17 -0300

    Connect and tests mongoose

--- Cursos/NestJS/nestjs-api-tests/src/tweets/entities/tweet,entity.spec.ts
+++ Cursos/NestJS/nestjs-api-tests/src/tweets/entities/tweet,entity.spec.ts
@@ -1,13 +1,46 @@
-import { Tweet } from "./tweet.entity"
+import mongoose from "mongoose";
+import { Tweet, TweetSchema } from "./tweet.entity"
 
 describe('Tweet Tests', () => {
-    it('Should create a tweet', () => {
-        const tweet = new Tweet({
-            content: 'Hello World',
-            screen_name: 'Felipe D. Santos'
+
+    describe('Tests Class', () => {
+        it('Should create a tweet', () => {
+            const tweet = new Tweet({
+                content: 'Hello World',
+                screen_name: 'Felipe D. Santos'
+            });
+    
+            expect(tweet.content).toBe('Hello World');
+            expect(tweet.screen_name).toBe('Felipe D. Santos')
+        });
+    });
+
+
+    describe('Tests Connect mongoose', () => {
+
+        let conn: mongoose.Mongoose
+
+        beforeEach(async () => {
+            conn = await mongoose.connect(
+                'mongodb://root:root@localhost:27017/tweets_test?authSource=admin',);
         });
 
-        expect(tweet.content).toBe('Hello World');
-        expect(tweet.screen_name).toBe('Felipe D. Santos')
-    })
+        afterEach(async () => {
+            await conn.disconnect();
+        });
+
+        it('Create a tweet document', async () => {
+            
+            const TweetModel = conn.model('Tweet', TweetSchema);
+            const tweet = new TweetModel({
+                content: 'Hello World',
+                screen_name: 'Felipe D. Santos'
+            });
+            await tweet.save();
+            const tweetCreated = await TweetModel.findById(tweet._id);
+            expect(tweetCreated.content).toBe('Hello World');
+            expect(tweetCreated.screen_name).toBe('Felipe D. Santos');
+        });
+    });
+
 })
--- Cursos/NestJS/nestjs-api-tests/src/tweets/entities/tweet.entity.ts
+++ Cursos/NestJS/nestjs-api-tests/src/tweets/entities/tweet.entity.ts
@@ -1,16 +1,23 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+
 
 export type TweetProps = {
     content: string;
     screen_name: string;
 }
 
+@Schema()
 export class Tweet {
-
+    
+    @Prop({ required: true })
     content: string;
+    @Prop({ required: true })
     screen_name: string;
-
+    
     constructor(props: TweetProps) {
         Object.assign(this, props)
     }
 
 }
+
+export const TweetSchema = SchemaFactory.createForClass(Tweet);
